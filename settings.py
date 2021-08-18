0f5d2914ca86fcf84a431a06a1f1a8a9e667665e
Alexandr9760 <80711815+Alexandr9760@users.noreply.github.com>
2021-08-18 22:21:17 +0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,5 @@
+valid_email = "alexandrn-mail@yandex.ru"
+valid_password = "Ly3571@"
+incorrect_password = "123"
+incorrect_email = "alexandr@maillex.ru"
+unlisted_ID = 'f1b2c449-a41b-43fe-a9f7-d5493e9dd911'
